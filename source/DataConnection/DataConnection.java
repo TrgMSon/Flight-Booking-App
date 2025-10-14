eb5a9276ad0ebe6eca909e0531b26a22b5059548
Trương Minh Sơn <163323332+TrgMSon@users.noreply.github.com>
2025-10-14 20:50:30 +0700

    Update DataConnection.java

--- source/DataConnection/DataConnection.java
+++ source/DataConnection/DataConnection.java
@@ -1,1 +1,24 @@
 
+package Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataConnection {
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/flightbookingdb?user=root";
+    private static final String user = "root";
+    private static final String password = "020677MSon@";
+
+    public static Connection setConnect() {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch(SQLException e) {
+            System.out.println("Error connect");
+        }
+
+        return conn;
+    }
+}
