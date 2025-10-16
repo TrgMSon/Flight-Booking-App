c7a0baa9054341bfa61744429a02e7930d80dbd1
Trương Minh Sơn <163323332+TrgMSon@users.noreply.github.com>
2025-10-16 21:16:31 +0700

    Update FormCustomer.java

--- source/UI/FormCustomer.java
+++ source/UI/FormCustomer.java
@@ -1,4 +1,5 @@
 package UI;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -22,8 +23,15 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import java.util.Random;
 import entity.Customer;
+import UI.Mode.Mode;
 
 public class FormCustomer extends Application {
+    private Mode mode;
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
+    }
+
     public void start(Stage stage) {
         BorderPane root = new BorderPane();
         root.setPadding(new Insets(20));
@@ -57,11 +65,11 @@ public class FormCustomer extends Application {
         buttonBox.setPadding(new Insets(50, 40, 20, 40));
 
         Button accept = new Button("Xác nhận");
-        initActionAccept(accept, stage, form);
+        initActionAccept(accept, stage, form, mode);
         Button close = new Button("Đóng");
         initActionClose(close, stage);
-        
-        buttonBox.getChildren().addAll(accept, close);  
+
+        buttonBox.getChildren().addAll(accept, close);
         form.getChildren().add(buttonBox);
 
         root.setTop(titlePane);
@@ -79,28 +87,29 @@ public class FormCustomer extends Application {
         Stage inform = new Stage();
         inform.setTitle("Thông báo");
         inform.initModality(Modality.APPLICATION_MODAL);
-        
+
         BorderPane root = new BorderPane();
-        VBox informBox = new VBox(20);
+        VBox informBox = new VBox(40);
         Label informLabel = new Label(message);
         Button close = new Button("Đóng");
         initActionClose(close, inform);
         informBox.setAlignment(Pos.CENTER);
         informBox.getChildren().addAll(informLabel, close);
-        
+
         root.setCenter(informBox);
         Scene scene = new Scene(root, 300, 200);
+        scene.getStylesheets().add(getClass().getResource("/effect/style.css").toExternalForm());
         inform.setScene(scene);
         inform.show();
     }
 
-    public void initActionAccept(Button accept, Stage stage, VBox form) {
+    public void initActionAccept(Button accept, Stage stage, VBox form, Mode mode) {
         accept.setFocusTraversable(false);
         accept.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent ae) {
                 HBox box = (HBox) form.getChildren().get(0);
                 String name = ((TextField) box.getChildren().get(1)).getText();
-                
+
                 box = (HBox) form.getChildren().get(1);
                 LocalDate localDate = ((DatePicker) box.getChildren().get(1)).getValue();
                 String dob = "";
@@ -114,19 +123,31 @@ public class FormCustomer extends Application {
                 box = (HBox) form.getChildren().get(3);
                 String mail = ((TextField) box.getChildren().get(1)).getText();
 
-                Random rand = new Random();
-                String customer_id = "C" + String.format("%04d", rand.nextInt(10000));
-                while (CustomerBusiness.isExist(customer_id)) {
-                    customer_id = "C" + String.format("%04d", rand.nextInt(10000));
-                }
-                
+                String customer_id = CustomerBusiness.showCustomerId(mail);
                 Customer customer = new Customer(customer_id, name, dob, phone, mail);
-                if (CustomerBusiness.isValid(customer)) {
-                    CustomerBusiness.addCustomer(customer);
-                }
-                else {
+                if (CustomerBusiness.isValid(customer) == false) {
                     initInform("Vui lòng nhập đủ thông tin khách hàng");
                     return;
+                }
+
+                if (customer_id.equals("")) {
+                    Random rand = new Random();
+                    customer_id = "C" + String.format("%04d", rand.nextInt(10000));
+                    while (CustomerBusiness.isExist(customer_id)) {
+                        customer_id = "C" + String.format("%04d", rand.nextInt(10000));
+                    }
+                    customer.setCustomerId(customer_id);
+                }
+
+                if (mode == Mode.ADD) {
+                    CustomerBusiness.addCustomer(customer);
+                }
+
+                else if (mode == Mode.SHOW) {
+                    Bill bill = new Bill();
+                    Stage stageBill = new Stage();
+                    bill.setCustomerId(CustomerBusiness.showCustomerId(mail));
+                    bill.start(stageBill);
                 }
 
                 stage.close();
@@ -151,7 +172,7 @@ public class FormCustomer extends Application {
         if (input instanceof TextField) {
             ((TextField) input).setFocusTraversable(false);
             ((TextField) input).setPrefSize(500, 40);
-        } 
+        }
 
         else if (input instanceof DatePicker) {
             ((DatePicker) input).setFocusTraversable(false);
